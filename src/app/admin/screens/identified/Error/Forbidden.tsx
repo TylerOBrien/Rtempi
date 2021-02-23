9ee9b508de33172b26099ad1504c6b58b2d7b01b
Tyler O'Brien <hello@tylerobrien.com>
2021-02-23 05:58:22 +0000

    Add placeholder Error screens

---
+++ src/app/admin/screens/identified/Error/Forbidden.tsx
@@ -0,0 +1,47 @@
+/**
+ * Global Imports
+*/
+
+import React, { ReactNode, useEffect, useContext, useState } from 'react';
+
+/**
+ * Local Imports
+*/
+
+import { Text, View } from '~/components/Base';
+
+/**
+ * Types/Interfaces
+*/
+
+export interface ForbiddenProps {
+  //
+}
+
+/**
+ * Locals
+*/
+
+
+
+/**
+ * Components
+*/
+
+export function Forbidden(props:ForbiddenProps) {
+  /** Contexts **/
+  
+  /** States **/
+  
+  /** Side-Effects **/
+  
+  /** Event Handlers **/
+  
+  /** Renderers **/
+  
+  /** Output **/
+  
+  return (
+    null
+  );
+}
---
+++ src/app/admin/screens/identified/Error/NotFound.tsx
@@ -0,0 +1,47 @@
+/**
+ * Global Imports
+*/
+
+import React, { ReactNode, useEffect, useContext, useState } from 'react';
+
+/**
+ * Local Imports
+*/
+
+import { Text, View } from '~/components/Base';
+
+/**
+ * Types/Interfaces
+*/
+
+export interface NotFoundProps {
+  //
+}
+
+/**
+ * Locals
+*/
+
+
+
+/**
+ * Components
+*/
+
+export function NotFound(props:NotFoundProps) {
+  /** Contexts **/
+  
+  /** States **/
+  
+  /** Side-Effects **/
+  
+  /** Event Handlers **/
+  
+  /** Renderers **/
+  
+  /** Output **/
+  
+  return (
+    null
+  );
+}
