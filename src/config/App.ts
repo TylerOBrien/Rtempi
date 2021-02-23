42c7d258c9e9d17e88d24da1f74259a6ec556b4e
Tyler O'Brien <hello@tylerobrien.com>
2021-02-23 21:50:07 +0000

    Add reactRootElementId to config

--- src/config/App.ts
+++ src/config/App.ts
@@ -5,5 +5,6 @@
 export const AppConfig = {
   name: process.env.APP_NAME,
   slug: process.env.APP_SLUG || (process.env.APP_NAME || '').toLowerCase().replace(/[^a-z\-]/g, ''),
-  debug: (process.env.APP_DEBUG || '').toLowerCase() === 'true'
+  debug: (process.env.APP_DEBUG || '').toLowerCase() === 'true',
+  reactRootElementId: 'root'
 };
