86eaf0db6dc16c6c7b622d139fe5b0fb3a776ee9
Tyler O'Brien <hello@tylerobrien.com>
2021-02-23 23:52:27 +0000

    Add placeholder routers

---
+++ src/app/user/routers/identified/IdentifiedRouter.tsx
@@ -0,0 +1,30 @@
+/**
+ * Global Imports
+*/
+
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+/**
+ * Local Imports
+*/
+
+
+
+/**
+ * Sibling Imports
+*/
+
+
+
+/**
+ * Exports
+*/
+
+export function IdentifiedRouter() {
+  return (
+    <Switch>
+      <Route path='' component={ null } />
+    </Switch>
+  );
+}
